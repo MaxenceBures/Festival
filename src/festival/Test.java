3d8a00fd42b4be9b2dde95aef11c05bf4ad8b89b
MaxenceBures <maxence.bures@gmail.com>
2013-03-27 16:20:18 +0100

    11

--- src/festival/Test.java
+++ src/festival/Test.java
@@ -13,7 +13,7 @@ public class Test {
     public static void main(String[] args) {
         // TODO code application logic here
         System.out.println("test122");
-        System.out.println("test32sfgrsg");
+        System.out.println("test32sfsdfgsgrsg");
 
     
     }
