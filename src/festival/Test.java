658c9ac5d865dce4d7b192cec6f5e269b6eb9095
MaxenceBures <maxence.bures@gmail.com>
2013-03-27 15:58:33 +0100

    9

---
+++ src/festival/Test.java
@@ -0,0 +1,20 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package festival;
+
+/**
+ *
+ * @author Maxence
+ */
+public class Test {
+   
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("test2");
+
+    
+    }
+    
+}
